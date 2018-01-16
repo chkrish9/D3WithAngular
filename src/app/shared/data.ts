655d822b67bb5c19896a01d7a9f3b9e02ec5c352
Murali <chkrish.9@dummy.com>
2018-01-16 19:10:19 +0530

    Pie chart phase1

---
+++ src/app/shared/data.ts
@@ -0,0 +1,9 @@
+export const Stats: any[] = [
+    {age: "10-1", population: 10},
+    {age: "30-2", population: 30},
+    {age: "20-3", population: 20},
+    {age: "50-4", population: 50},
+    {age: "10-5", population: 10},
+    {age: "20-6", population: 20},
+    {age: "40-7", population: 40}
+  ];
